e5c46326944c32989ebf4e502b7f861ce3b51628
Geovanny Mendoza <mgeovanny@uninorte.edu.co>
2025-01-22 23:15:25 -0500

    V[0.0.4] Update code

--- src/main/frontend/views/@index.tsx
+++ src/main/frontend/views/@index.tsx
@@ -7,7 +7,6 @@ import { EmployeeService } from 'Frontend/generated/endpoints';
 export default function Employees() {
     const employees = useSignal<Employee[]>([]);
 
-    // Función para obtener la lista de empleados
     async function fetchEmployees() {
         const fetchedEmployees = await EmployeeService.findAll();
         employees.value = (fetchedEmployees || []).filter((employee): employee is Employee => employee !== undefined);
@@ -16,6 +15,12 @@ export default function Employees() {
     useEffect(() => {
         fetchEmployees();
     }, []);
+
+    function ActiveRenderer({ item }: { item: Employee }) {
+        const { active } = item;
+        const color = active ? 'green' : 'red';
+        return <span style={{ fontWeight: 'bold', color }}>{active ? 'Yes' : 'No'}</span>;
+    }
 
     return (
         <div className="p-m flex flex-col gap-m">
@@ -28,7 +33,7 @@ export default function Employees() {
                 <GridSortColumn path="phone" header="Teléfono" />
                 <GridSortColumn path="startDate" header="Fecha de Inicio" />
                 <GridSortColumn path="shiftStartsAt" header="Inicio del Turno" />
-                <GridSortColumn path="active" header="Activo" />
+                <GridSortColumn path="active" header="Activo" renderer={ActiveRenderer}/>
                 <GridSortColumn path="description" header="Descripción" />
             </Grid>
         </div>
